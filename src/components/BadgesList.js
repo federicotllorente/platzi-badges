563aaf38c7fc197e0996a2de846c49e8452ff5c5
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-02 09:25:48 +0100

    Now the app manages if there's an error or the data when it's fetching it. Also, if it's loading searching for the information

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import twitterLogo from '../img/twitter.svg';
 
 class BadgesList extends Component {
     render() {
+        if (this.props.isLoading) {
+            return (
+                <div className="BadgesList">
+                    <ul>
+                        <li className="BadgesList__item">
+                            <img className="BadgesList__item__avatar" src="https://mir-s3-cdn-cf.behance.net/user/276/2aa2be101429359.5fe347ac143e2.png" alt="Avatar" />
+                            <div className="BadgesList__item__content">
+                                <h2>Nombre Apellido</h2>
+                                <p>
+                                    <img src={twitterLogo} alt="Twitter Logo" />
+                                    <a href={"https://www.twitter.com/federicotllorente/"}>@federicotllorente</a>
+                                </p>
+                                <h3>Job title</h3>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            );
+        }
+        if (this.props.listData.length === 0) {
+            return (
+                <div className="Homepage__content">
+                    <h1>There's no badges yet</h1>
+                    <p>Do you want to create a new one? Let's try out!</p>
+                    <Link to="/badges/new">Create a new Badge</Link>
+                </div>
+            );
+        }
         return (
             <div className="BadgesList">
                 <ul>
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -2,41 +2,45 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import confLogo from '../img/badge-header.svg';
+import mainLogo from '../img/mainLogo.svg';
 import BadgesList from '../components/BadgesList';
+
+import api from '../api';
 
 class Badges extends Component {
     state = {
-        data: [
-            {
-                id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-                fname: "Freda",
-                lname: "Grady",
-                email: "Leann_Berge@gmail.com",
-                jtitle: "Legacy Brand Director",
-                twitter: "FredaGrady22221-7573",
-                avatarUrl: "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-            },
-            {
-                id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-                fname: "Major",
-                lname: "Rodriguez",
-                email: "Ilene66@hotmail.com",
-                jtitle: "Human Research Architect",
-                twitter: "ajorRodriguez61545",
-                avatarUrl: "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-            },
-            {
-                id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-                fname: "Daphney",
-                lname: "Torphy",
-                email: "Ron61@hotmail.com",
-                jtitle: "National Markets Officer",
-                twitter: "DaphneyTorphy96105",
-                avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-            }
-        ]
+        loading: true,
+        data: undefined,
+        error: null
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+    fetchData = async () => {
+        this.setState({ loading: true, error: null });
+        try {
+            const data = await api.badges.list();
+            this.setState({ loading: false, data: data });
+        } catch (error) {
+            this.setState({ loading: false, error: error });
+        }
     }
     render() {
+        // if (this.state.loading) {
+        //     return "Loading...";
+        // }
+        if (this.state.error) {
+            return (
+                <div className="NotFound">
+                    <img src={mainLogo} alt="Platzi Badges Logo" />
+                    <h1>Oops!</h1>
+                    <p style={{ textAlign: 'center' }}>
+                        There's an error...<br />Please, try again later 😊
+                    </p>
+                    <Link to="/">Return to the Homepage</Link>
+                </div>
+            );
+        }
         return (
             <div className="Badges">
                 <div className="Badges__hero">
@@ -48,7 +52,7 @@ class Badges extends Component {
                     <div className="Badges__buttons">
                         <Link to="/badges/new">Add</Link>
                     </div>
-                    <BadgesList listData={this.state.data} />
+                    <BadgesList listData={this.state.data} isLoading={this.state.loading} />
                 </div>
             </div>
         );
