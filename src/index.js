612267292831350d098945720ef4d657ec6ff04b
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-04-02 15:31:38 +0200

    Starting to integrate Redux and React-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './styles/main.css';
 import App from './components/App';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Provider><App /></Provider>, container);
