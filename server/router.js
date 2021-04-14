0e77ebad466af7ea14366915c6202864fa8d3ed2
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-04-14 21:34:33 +0200

    HTTP requests managed with a .then() and .catch()

--- server/router.js
+++ server/router.js
@@ -4,38 +4,70 @@ const router = express.Router();
 const pool = require('./db');
 
 router.get('/badges', async (req, res) => {
-    const badges = await pool.query('SELECT * FROM badges');
-    res.send(badges);
+    const badges = await pool.query('SELECT * FROM badges')
+        .then(data => {
+            console.log(data);
+            return res.status(200).send(badges);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).send('[router] There was an error trying to get the badges');
+        });
 });
 
 router.get('/badges/:badgeId', async (req, res) => {
-    const requestedBadge = await pool.query('SELECT * FROM badges WHERE id = ?', [req.params.badgeId]);
-    res.send(requestedBadge[0]);
+    const requestedBadge = await pool.query('SELECT * FROM badges WHERE id = ?', [req.params.badgeId])
+        .then(data => {
+            console.log(data);
+            return res.status(200).send(requestedBadge[0]);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).send(`[router] There was an error trying to get the badge ${req.params.badgeId}`);
+        });
 });
 
 router.post('/badges', async (req, res) => {
     const { fname, lname, email, jtitle, twitter } = req.body;
     const newBadge = { fname, lname, email, jtitle, twitter };
-    await pool.query('INSERT INTO badges set ?', [newBadge]);
-    console.log('Badge succesfully created!');
-    console.log(newBadge);
-    res.send(newBadge);
+    await pool.query('INSERT INTO badges set ?', [newBadge])
+        .then(data => {
+            console.log('Badge succesfully created!');
+            console.log(data);
+            return res.status(201).send(newBadge);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).send('[router] There was an error trying to add a badge');
+        });
 });
 
 router.put('/badges/:badgeId', async (req, res) => {
     const { fname, lname, email, jtitle, twitter } = req.body;
     const updatedBadge = { fname, lname, email, jtitle, twitter };
-    await pool.query('UPDATE badges set ? WHERE id = ?', [updatedBadge, req.params.badgeId]);
-    console.log('Badge succesfully updated!');
-    console.log(updatedBadge);
-    res.send(updatedBadge);
+    await pool.query('UPDATE badges set ? WHERE id = ?', [updatedBadge, req.params.badgeId])
+        .then(data => {
+            console.log('Badge succesfully updated!');
+            console.log(data);
+            return res.status(200).send(updatedBadge);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).send('[router] There was an error trying to add a badge');
+        });
 });
 
 router.delete('/badges/:badgeId', async (req, res) => {
-    const badgeToDelete = await pool.query('DELETE FROM badges WHERE id = ?', [req.params.badgeId]);
-    console.log('Badge succesfully deleted');
-    console.log(badgeToDelete);
-    res.send(badgeToDelete);
+    const badgeToDelete = await pool.query('DELETE FROM badges WHERE id = ?', [req.params.badgeId])
+        .then(data => {
+            console.log('Badge succesfully deleted');
+            console.log(data);
+            return res.status(200).send(badgeToDelete);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).send('[router] There was an error trying to add a badge');
+        });
 });
 
 module.exports = router;
